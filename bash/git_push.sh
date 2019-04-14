5bfff3a3b7127ed89477fafa37b496426352620a
Kawsay <cc.lle@protonmail.com>
2019-04-14 23:31:27 +0200

    0

--- bash/git_push.sh
+++ bash/git_push.sh
@@ -10,6 +10,12 @@ then
   git push
   git status
 else
+  while [ $# -ne 0 ]
+    do
+      echo "Current Parameter: $1 , Remaining $#"
+      #Pass $1 to some bash function or do whatever
+      shift
+  done
   git add .
   git commit -m $#
   git push
