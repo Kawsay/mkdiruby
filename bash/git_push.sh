8a6defa4527e7fe74f481b9f14605f08281d1486
Kawsay <cc.lle@protonmail.com>
2019-04-14 23:24:17 +0200

    3

--- bash/git_push.sh
+++ bash/git_push.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 
-read -p "Lovely comment here : " comment
-
-git add .
-git commit -m $comment
-git push
-git status
+# Check/get arguments
+if [ $# -eq 0 ]
+then
+  echo "Lovely comment :"
+  read -e comment
+  git add .
+  git commit -m $comment
+  git push
+  git status
+else
+  git add .
+  git commit -m $#
+  git push
+  git status
+fi
